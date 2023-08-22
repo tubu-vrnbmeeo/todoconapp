e2f7c8f943d69ef2cca8a561f1acbb4f1018ba4f
tubu-vrnbmeeo <takahiro.ubukata1123@gmail.com>
2023-08-22 23:34:25 +0900

    edit button, update method

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -19,7 +19,17 @@ class BoardsController < ApplicationController
   end
 
   def edit
-    @board = current_user.article.find(params[:id])
+    @board = current_user.boards.find(params[:id])
+  end
+
+  def update
+    @board = current_user.boards.find(params[:id])
+    if @board.update(board_params)
+      redirect_to root_path, notice: 'Updated'
+    else
+      flash.now[:error] = 'Not updated'
+      render :edit
+    end
   end
 
   private
