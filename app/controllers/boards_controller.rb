fe7dca4beafd69b82fcc44c5f84d49ea6b5532bb
tubu-vrnbmeeo <takahiro.ubukata1123@gmail.com>
2023-08-22 10:38:20 +0900

    createのcontroller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,9 +1,25 @@
 class BoardsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @boards = Board.all
   end
 
   def new
     @board = current_user.boards.build
+  end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to board_path, notice: '保存しました'
+    else
+      flash.now[:error] = '保存できませんでした'
+      render :new
+    end
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:name, :description)
   end
 end
