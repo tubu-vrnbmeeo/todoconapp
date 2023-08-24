0a939f990c0914ac3d1c5d49dd8268f5b759c42f
tubu-vrnbmeeo <takahiro.ubukata1123@gmail.com>
2023-08-24 22:25:45 +0900

    task model作成、migration, routes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,19 +1,18 @@
 # == Schema Information
 #
-# Table name: boards
+# Table name: tasks
 #
 #  id          :bigint           not null, primary key
 #  description :text             not null
 #  name        :string           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
-#  user_id     :bigint           not null
+#  board_id    :bigint           not null
 #
 # Indexes
 #
-#  index_boards_on_user_id  (user_id)
+#  index_tasks_on_board_id  (board_id)
 #
-class Board < ApplicationRecord
-  belongs_to :user
-  has_many :tasks
+class Task < ApplicationRecord
+  belongs_to :board
 end
---
+++ db/migrate/20230824130836_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.references :board, null: false
+      t.string :name, null: false
+      t.text :description, null: false
+      t.timestamps
+    end
+  end
+end
