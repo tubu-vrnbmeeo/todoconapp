c4b79c85b3568c0497c5f22de7fa224c388351b3
tubu-vrnbmeeo <takahiro.ubukata1123@gmail.com>
2023-08-30 22:46:14 +0900

    ボタンcssの変更、comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+  def new
+    task = Board.find(params[:board_id]).tasks.find(params[:task_id])
+    @comment = task.comments.build
+  end
+
+  def create
+    task = Board.find(params[:board_id]).tasks.find(params[:task_id])
+    @comment = task.comments.build(**comment_params, user_id: current_user.id)
+    if @comment.save
+      redirect_to board_task_path(board_id: task.board.id, id: task.id), notice: 'Comment added'
+    else
+      flash.now[:error] = 'Comment could not be added'
+      render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end
