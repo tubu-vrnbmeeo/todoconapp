2e6ee1bf284c2a510b013a5556fb76eed8810246
tubu-vrnbmeeo <takahiro.ubukata1123@gmail.com>
2023-09-03 18:05:38 +0900

    model作成, active record, routes, controllerの空箱

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_one :profile, dependent: :destroy
   has_many :boards, dependent: :destroy
   has_many :tasks, dependent: :destroy
   has_many :comments, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
       resources :comments, only: [:new, :create]
     end
   end
+
+  resouce :profile, only: [:show, :edit, :update]
 end
