bf2b793ac55d2cfa63c31840c58db3d7946fd0c3
tubu-vrnbmeeo <takahiro.ubukata1123@gmail.com>
2023-09-03 22:45:10 +0900

    update

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,17 +1,30 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_profile, only: [:edit, :update]
+
   def show
     @profile = current_user.profile
   end
 
   def edit
-    @profile = current_user.prepare_profile
   end
 
   def update
+    @profile.assign_attributes(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'Profile updated'
+    else
+      flash.now[:error] = 'Profile could not updated'
+      render :edit
+    end
   end
 
   private
   def profile_params
+    params.require(:profile).permit(:user_name, :introduction, :avatar)
+  end
+
+  def set_profile
+    @profile = current_user.prepare_profile
   end
 end
